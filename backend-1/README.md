3ee06b7c73783bc9fb76705dbd96c1eeb28f2f46
EniyaGanesan <122864702+EniyaGanesan@users.noreply.github.com>
2024-07-18 19:03:29 +0530

    Delete backend-1 directory

--- backend-1/README.md
+++
@@ -1,8 +0,0 @@
-
-1) Run the following sql quires in mysql
-
-CREATE DATABASE user_auth_db;
-use user_auth_db;
-
-
-2) run : node server.js
--- backend-1/middleware/checkLock.js
+++
@@ -1,9 +0,0 @@
-const User = require('../models/user.model');
-const checkLock = async (req, res, next) => {
-    const user = await User.findOne({where: {username: req.body.username}});
-    if (user && user.locked) {
-        return res.status(403).send({data:'Account is locked.'});
-    }
-    next();
-};
-module.exports = checkLock;
